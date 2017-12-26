87be54a99e0874d31e23c50e002859d3fd25bb45
mcappa <mattia.cappa@nttdata.com>
2017-12-26 16:52:13 +0100

    Calcolo di accuratezza usando decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -11,7 +11,8 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
 
 
 ### features_train and features_test are the features for the training
@@ -24,7 +25,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+print 'Number of features: {}'.format(len(features_train[0]))
 
+clf = DecisionTreeClassifier(min_samples_split=40)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t1 = time()
+prediction = clf.predict(features_test)
+print "prediction time:", round(time() - t1, 3), "s"
+
+accuracy = clf.score(features_test, labels_test)
+
+print 'Accuracy: {}'.format(accuracy)
 
 #########################################################
 
