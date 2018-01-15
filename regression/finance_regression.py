70f8e669026fd583a64204112d7da0923b1ce29a
mcappa <mattia.cappa@nttdata.com>
2018-01-15 01:08:15 +0100

    esercizio sulle regression line

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -16,12 +16,13 @@
 import sys
 import pickle
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+#features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,6 +38,15 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
+
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print reg.coef_
+print reg.intercept_
+
+print "Score: ", reg.score(feature_train, target_train)
+print "Score: ", reg.score(feature_test, target_test)
 
 
 
@@ -64,6 +74,13 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print reg.coef_
+print "Score: ", reg.score(feature_train, target_train)
+print "Score: ", reg.score(feature_test, target_test)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
