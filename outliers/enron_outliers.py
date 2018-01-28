d531a14fd9f1c89ac7e8d791bab565801e9172b6
mcappa <mattia.cappa@nttdata.com>
2018-01-28 22:25:08 +0100

    remove wrong data from spreadsheet

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -3,17 +3,32 @@
 import pickle
 import sys
 import matplotlib.pyplot
+from operator import itemgetter
+
+from sklearn import linear_model
+
 sys.path.append("../tools/")
-from feature_format import featureFormat, targetFeatureSplit
+from tools.feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop('TOTAL', 0) # Added after finding the outlier
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
 
